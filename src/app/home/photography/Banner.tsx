4e2193cae2372f7203d4dc2f583038a9b70252dc
BodaciousTea <tebodacious@gmail.com>
2024-01-04 22:59:14 -0600

    /Mobile added

--- src/app/home/photography/Banner.tsx
+++ src/app/home/photography/Banner.tsx
@@ -4,17 +4,17 @@ import Carousel from '@/components/common/Carousel';
 function Banner() {
   const carouselItems = [
     {
-      imageUrl: '/images/carouselMobile1.png',
+      imageUrl: '/Mobile/images/carouselMobile1.png',
       titleStyle: { background: 'linear-gradient(to right, #43e97b 0%, #38f9d7 100%)' },
       onButtonClick: () => window.open('https://www.tedkoller.com/gallery', '_blank')
     },
     {
-      imageUrl: '/images/carouselMobile2.png',
+      imageUrl: '/Mobile/images/carouselMobile2.png',
       titleStyle: { background: 'linear-gradient(to right, #ff758c 0%, #ff7eb3 100%)' },
       onButtonClick: () => {/* handle click for Web Design */}
     },
     {
-      imageUrl: '/images/carouselMobile3.png',
+      imageUrl: '/Mobile/images/carouselMobile3.png',
       titleStyle: { background: 'linear-gradient(to right, #667eea 0%, #764ba2 100%)' },
       onButtonClick: () => {/* handle click for Video Production */}
     }
